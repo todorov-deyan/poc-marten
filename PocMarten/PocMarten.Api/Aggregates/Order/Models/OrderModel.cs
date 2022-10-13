be668977e6f9eed3af48d97b5c511a831232154f
Deyan Todorov <deyan.todorov@endava.com>
2022-10-13 15:07:33 +0300

    fix my order broken controller

---
+++ PocMarten/PocMarten.Api/Aggregates/Order/Models/OrderModel.cs
@@ -0,0 +1,24 @@
+﻿using PocMarten.Api.Aggregates.Order.Events;
+using PocMarten.Api.Common.EventSourcing;
+
+namespace PocMarten.Api.Aggregates.Order.Models
+{
+    public class OrderModel : Aggregate
+    {
+        public Priority Priority { get; set; }
+
+        public string CustomerId { get; set; }
+
+        public IList<OrderDetail> Details { get; set; } = new List<OrderDetail>();
+
+        public void Apply(OrderPriorityHigh @event)
+        {
+            Priority = @event.Priority;
+        }
+
+        public void Apply(OrderPriorityLow @event)
+        {
+            Priority = @event.Priority;
+        }
+    }
+}
