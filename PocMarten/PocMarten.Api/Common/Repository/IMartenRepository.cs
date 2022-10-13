1f354cc7b84df7e0fe6a6db74281de08a054d51a
Deyan Todorov <deyan.todorov@endava.com>
2022-10-13 15:04:32 +0300

    namespace fix

--- PocMarten/PocMarten.Api/Common/Repository/IMartenRepository.cs
+++ PocMarten/PocMarten.Api/Common/Repository/IMartenRepository.cs
@@ -5,6 +5,7 @@ namespace PocMarten.Api.Common.Repository
     public interface IMartenRepository<T> where T : class, IAggregate
     {
         Task<T?> Find(Guid id, CancellationToken cancellationToken);
+
         Task Add(T aggregate, IList<IEventState> events, CancellationToken cancellationToken = default);
 
         Task Update(Guid id, IList<IEventState> events, CancellationToken cancellationToken = default);
