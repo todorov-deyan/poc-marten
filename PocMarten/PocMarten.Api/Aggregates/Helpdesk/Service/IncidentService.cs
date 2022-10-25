b882370190c48f86ff1a4d74af859937549f77ed
Deyan Todorov <deyan.todorov@endava.com>
2022-10-25 17:28:02 +0300

    service & controller

---
+++ PocMarten/PocMarten.Api/Aggregates/Helpdesk/Service/IncidentService.cs
@@ -0,0 +1,15 @@
+﻿using PocMarten.Api.Aggregates.Helpdesk.Events;
+using PocMarten.Api.Aggregates.Helpdesk.Service.Models;
+
+namespace PocMarten.Api.Aggregates.Helpdesk.Service
+{
+    public static class IncidentService
+    {
+        public static IncidentLogged Handle(LogIncident command)
+        {
+            var (incidentId, customerId, contact, description, loggedBy) = command;
+
+            return new IncidentLogged(incidentId, customerId, contact, description, loggedBy, DateTimeOffset.UtcNow);
+        }
+    }
+}
---
+++ PocMarten/PocMarten.Api/Controllers/HelpdeskController.cs
@@ -0,0 +1,38 @@
+﻿using Marten;
+using Marten.Schema.Identity;
+
+using Microsoft.AspNetCore.Mvc;
+
+using PocMarten.Api.Aggregates.Helpdesk.Core.Marten;
+using PocMarten.Api.Aggregates.Helpdesk.Dto;
+using PocMarten.Api.Aggregates.Helpdesk.Models;
+
+using static PocMarten.Api.Aggregates.Helpdesk.Service.IncidentService;
+
+namespace PocMarten.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HelpdeskController : ControllerBase
+    {
+        private readonly IDocumentSession _documentSession;
+
+        public HelpdeskController(IDocumentSession documentSession)
+        {
+            _documentSession = documentSession;
+        }
+
+        [HttpPost]
+        [Route("api/customers/{customerId:guid}/incidents/")]
+        public async Task<IActionResult> Post([FromRoute] Guid customerId, [FromBody] LogIncidentRequest body, CancellationToken cancellationToken)
+        {
+            var (contact, description) = body;
+            var incidentId = CombGuidIdGeneration.NewGuid();
+
+            await _documentSession.Add<Incident>(incidentId,
+                Handle(new Aggregates.Helpdesk.Service.Models.LogIncident(incidentId, customerId, contact, description, customerId)), cancellationToken);
+
+            return Created($"/api/incidents/{incidentId}", incidentId);
+        }
+    }
+}
