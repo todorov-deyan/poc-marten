162fb844458c89bbe5b85fe5345f2a28b8fb205e
Deyan Todorov <deyan.todorov@endava.com>
2022-10-26 12:52:31 +0300

    simple cancellationToken test

---
+++ PocMarten/PocMarten.Api/Controllers/SlowRequestController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PocMarten.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SlowRequestController : ControllerBase
+    {
+        private readonly ILogger _logger;
+
+        public SlowRequestController(ILogger<SlowRequestController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("/slowtest")]
+        public async Task<string> Get()
+        {
+            _logger.LogInformation("Starting to do slow work");
+
+            // slow async action, e.g. call external api
+            await Task.Delay(10_000);
+
+            var message = "Finished slow delay of 10 seconds.";
+
+            _logger.LogInformation(message);
+
+            return message;
+        }
+
+        [HttpGet("/slowtest-with-cancellation-token")]
+        public async Task<string> GetWithCancellationToken(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("Starting to do slow work");
+
+            // slow async action, e.g. call external api
+            await Task.Delay(10_000, cancellationToken);
+
+            var message = "Finished slow delay of 10 seconds.";
+
+            _logger.LogInformation(message);
+
+            return message;
+        }
+    }
+}
